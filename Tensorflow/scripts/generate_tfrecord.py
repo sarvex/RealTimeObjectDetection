9347aa00417daf5c32974d4b2cf5de4582fd08e8
Sourcery AI <>
2023-05-12 06:15:55 +0000

    'Refactored by Sourcery'

--- Tensorflow/scripts/generate_tfrecord.py
+++ Tensorflow/scripts/generate_tfrecord.py
@@ -93,8 +93,7 @@ def xml_to_csv(path):
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
                    'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    xml_df = pd.DataFrame(xml_list, columns=column_name)
-    return xml_df
+    return pd.DataFrame(xml_list, columns=column_name)
 
 
 def class_text_to_int(row_label):
@@ -108,7 +107,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.gfile.GFile(os.path.join(path, f'{group.filename}'), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -131,21 +130,36 @@ def create_tf_example(group, path):
         classes_text.append(row['class'].encode('utf8'))
         classes.append(class_text_to_int(row['class']))
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
-    return tf_example
+    return tf.train.Example(
+        features=tf.train.Features(
+            feature={
+                'image/height': dataset_util.int64_feature(height),
+                'image/width': dataset_util.int64_feature(width),
+                'image/filename': dataset_util.bytes_feature(filename),
+                'image/source_id': dataset_util.bytes_feature(filename),
+                'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+                'image/format': dataset_util.bytes_feature(image_format),
+                'image/object/bbox/xmin': dataset_util.float_list_feature(
+                    xmins
+                ),
+                'image/object/bbox/xmax': dataset_util.float_list_feature(
+                    xmaxs
+                ),
+                'image/object/bbox/ymin': dataset_util.float_list_feature(
+                    ymins
+                ),
+                'image/object/bbox/ymax': dataset_util.float_list_feature(
+                    ymaxs
+                ),
+                'image/object/class/text': dataset_util.bytes_list_feature(
+                    classes_text
+                ),
+                'image/object/class/label': dataset_util.int64_list_feature(
+                    classes
+                ),
+            }
+        )
+    )
 
 
 def main(_):
@@ -158,10 +172,10 @@ def main(_):
         tf_example = create_tf_example(group, path)
         writer.write(tf_example.SerializeToString())
     writer.close()
-    print('Successfully created the TFRecord file: {}'.format(args.output_path))
+    print(f'Successfully created the TFRecord file: {args.output_path}')
     if args.csv_path is not None:
         examples.to_csv(args.csv_path, index=None)
-        print('Successfully created the CSV file: {}'.format(args.csv_path))
+        print(f'Successfully created the CSV file: {args.csv_path}')
 
 
 if __name__ == '__main__':
